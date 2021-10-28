3b77aed9448ac62cbde7d896ed0f370d1f11d741
morganwilde <morgan@wilde.work>
2021-10-28 23:14:05 +0300

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const packageJSON = require('./package.json');
+
+const configuration = {
+  mode: 'production',
+  entry: [
+    './source/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'distribution'),
+    filename: 'bundle.js',
+    library: {
+      name: packageJSON.name,
+      type: 'umd'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        resolve: {
+          fullySpecified: false
+        },
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  resolve: {
+    fallback: {
+      'http': require.resolve('stream-http'),
+      'https': require.resolve('https-browserify'),
+      'url': require.resolve('url/'),
+      'buffer': require.resolve('buffer/')
+    },
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react/')
+    }
+  },
+  externals: {
+    react: 'react'
+  },
+};
+
+module.exports = configuration;
